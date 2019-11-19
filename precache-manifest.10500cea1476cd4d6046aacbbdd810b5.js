4fe7724f54421164fe9beae7e55449b78b7e0315
ecocyril <ecocyril@gmail.com>
2019-11-19 16:45:17 +0800

    Updates

---
+++ precache-manifest.10500cea1476cd4d6046aacbbdd810b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7c3d4c60d73f667e6e580730d6d7da",
+    "url": "/toy-app/index.html"
+  },
+  {
+    "revision": "8a3b8ffa68ab450361ca",
+    "url": "/toy-app/static/css/2.ed45abbb.chunk.css"
+  },
+  {
+    "revision": "dc99a9bb168dec0b381e",
+    "url": "/toy-app/static/css/main.6fdcdd19.chunk.css"
+  },
+  {
+    "revision": "8a3b8ffa68ab450361ca",
+    "url": "/toy-app/static/js/2.b05b9300.chunk.js"
+  },
+  {
+    "revision": "dc99a9bb168dec0b381e",
+    "url": "/toy-app/static/js/main.9ee72838.chunk.js"
+  },
+  {
+    "revision": "9dfde0d48aff7827b688",
+    "url": "/toy-app/static/js/runtime-main.2af90299.js"
+  }
+]);
